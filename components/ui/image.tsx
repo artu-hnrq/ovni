97ce6f6fcd2115373e8b5c4ec1093b399a44eb3f
Arthur Henrique <Arthur.Henrique.Della.Fraga@gmail.com>
2023-05-23 05:37:42 -0300

    fix: adjust image component cover behavior

--- components/ui/image.tsx
+++ components/ui/image.tsx
@@ -1,6 +1,7 @@
 import NextImage, { ImageProps as NextImageProps } from 'next/image'
 import { AspectRatio } from "@/components/ui/aspect-ratio"
 
+import { cn } from "@/lib/utils"
 
 export interface ImageProps extends NextImageProps {
     ratio: number
@@ -10,7 +11,10 @@ export default function Image({ className, ratio, ...props }: ImageProps) {
     return (
         <AspectRatio asChild ratio={ratio}>
             <NextImage
-                className={className}
+                className={cn(
+                    "h-full w-full object-cover",
+                    className,
+                )}
                 {...props}
                 fill
             />
