895f90b071bda7280a264f4702132120265c078e
Arthur Henrique <Arthur.Henrique.Della.Fraga@gmail.com>
2023-05-27 19:51:44 -0300

    feat: include ovni svg icon

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,12 @@ import Link from "next/link"
 
 import { siteConfig } from "@/config/site"
 import { buttonVariants } from "@/components/ui/button"
+import { Icons } from "@/components/icons"
 
 export default function IndexPage() {
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-
+      <Icons.Ovni />
     </section>
   )
 }
