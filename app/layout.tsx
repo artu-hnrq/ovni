e2b6f030baab9a00a1370750af4990f5b830b39e
Arthur Henrique <Arthur.Henrique.Della.Fraga@gmail.com>
2023-05-28 07:23:36 -0300

    build: bump next && adopt sass

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,9 @@
-import "@/styles/globals.css"
+import './globals.sass'
 import { Metadata } from "next"
 
 import { siteConfig } from "@/config/site"
 import { fontSans } from "@/lib/fonts"
 import { cn } from "@/lib/utils"
-import { SiteHeader } from "@/components/site-header"
 import { Sidebar } from "@/components/sidebar"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { ThemeProvider } from "@/components/theme-provider"
